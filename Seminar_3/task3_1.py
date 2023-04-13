046b9e6ad10102d83ff0c4a30402c49c0e5c9556
DmitryDW <DmitryDW1@yandex.ru>
2023-04-13 14:14:33 +0600

    Change task3_1

--- Seminar_3/task3_1.py
+++ Seminar_3/task3_1.py
@@ -2,5 +2,5 @@
 Задача No17. Дан список чисел. Определите, сколько в нем встречается
 различных чисел.
 '''
-numbers = [input () for i in range(5)]
+numbers = [input () for i in range(6)]
 print(len(set(numbers)))
