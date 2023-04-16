5d1d9a4feff6f594eff3b0126dce54a137b224d3
DmitryDW <DmitryDW1@yandex.ru>
2023-04-16 11:08:51 +0600

    Add Seminar_3

--- Seminar_3/task3_3.py
+++ Seminar_3/task3_3.py
@@ -6,8 +6,9 @@
 list_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, 
              {"VI": "S005"}, {"VII": " S005 "}, 
              {" V ":" S009 "}, {" VIII ":" S007 "}]
-unique_dict_items = set()
-for i in list_dict:
-    for j in i.keys():
-        unique_dict_items.add(i[j])
-print(unique_dict_items)
+# unique_dict_items = set()
+# for i in list_dict:
+#     for j in i.keys():
+#         unique_dict_items.add(i[j])
+# print(unique_dict_items)
+print(set([list(i.values())[0] for i in list_dict]))
--- Seminar_3/task3_4.py
+++ Seminar_3/task3_4.py
@@ -6,9 +6,9 @@
 
 print('Введите несколько чисел: ')
 numbers = [input() for i in range(5)]
-print(numbers)
-count = 0
-for i in range(len(numbers) - 1):
-    if numbers[i] < numbers[i + 1]: count += 1
-print(count)
-    
+# print(numbers)
+# count = 0
+# for i in range(len(numbers) - 1):
+#     if numbers[i] < numbers[i + 1]: count += 1
+# print(count) 
+print(sum([numbers[i] > numbers[i - 1] for i in range(1, len(numbers))]))
