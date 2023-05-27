0880a4f86441d8d352c2eccaa46bfe8a8c24037a
DmitryDW1 <118869480+DmitryDW1@users.noreply.github.com>
2023-05-27 07:41:51 +0600

    Add files via upload

--- Seminar_7/HomeWork/hw_task7_2.py
+++ Seminar_7/HomeWork/hw_task7_2.py
@@ -19,9 +19,9 @@
 6 12 18 24 30 36
 '''
 
-def print_operation_table(operation, num_rows, num_columns):   
+def print_operation_table(operation, num_rows = 6, num_columns = 6):   
     for i in range(1, num_rows + 1):
         print()
         for j in range(1, num_columns + 1 ): print(operation(i, j), end="\t")
 
-print_operation_table(lambda x, y: x * y, 6, 6)
+print_operation_table(lambda x, y: x * y)
